95de26265913764470a212c94a6a9876601be9b0
chsafouane <chsafouane@gmail.com>
2021-08-04 12:43:22 +0200

    MODIF] Added type doc to the input

--- cli.py
+++ cli.py
@@ -23,5 +23,17 @@ def goodbye_world(name):
     print(f"goodbye {name}!")
 
 
+@app.command()
+def add(n1: int = typer.Argument(..., help="An integer"), 
+        n2: int = typer.Argument(..., help="An integer")):
+    """Add two numbers
+
+    Args:
+        n1 (Int): First number
+        n2 (Int): Second number
+    """
+    print(n1 + n2)
+
+
 if __name__ == "__main__":
     app()
